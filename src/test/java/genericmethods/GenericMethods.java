9744e057a07ad3ae0a9ba037831d537c3e635277
Vishal Kudelli <Vishal>
2020-07-08 11:24:09 +0530

    Selenium

---
+++ src/test/java/genericmethods/GenericMethods.java
@@ -0,0 +1,41 @@
+package genericmethods;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GenericMethods {
+    public static WebDriver driver;
+
+    public void launchBrowser(String browserType, String url){
+
+        switch(browserType.toUpperCase()){
+
+            case "CHROME":
+                //logic for chrome
+                WebDriverManager.chromedriver().setup();
+                driver= new ChromeDriver();
+                break;
+
+            case "FF":
+                //logic for firefox
+                WebDriverManager.firefoxdriver().setup();
+                driver=new FirefoxDriver();
+                break;
+
+            case "EDGE":
+                //logic for Edge
+                WebDriverManager.edgedriver().setup();
+                driver=new EdgeDriver();
+                break;
+
+            default:
+                System.out.println("Invalid Browser");
+        }
+        driver.get(url);
+        driver.manage().window().maximize();
+    }
+
+}
